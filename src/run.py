5a59176d4df467bbea521dd0f81a15832ad42d31
Anton <piunov.doc@gmail.com>
2022-12-29 01:00:15 +0300

    [fix]: новый префикс

--- src/run.py
+++ src/run.py
@@ -26,7 +26,7 @@ class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-        super().__init__(command_prefix='!', intents=intents)
+        super().__init__(command_prefix='bro, ', intents=intents)
 
     async def setup_hook(self):
         await self.tree.sync(guild=MY_GUILD)
